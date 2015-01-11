f51cf3d3755e7b87dab8507bc6ce942e77554e30
niwacchi <niwacchi@gmail.com>
2015-01-11 11:44:20 +0900

    高負荷テスト用のリクエスト送信ツールを作成しました。

---
+++ RequestTest.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var request = require('request');
+var url = 'http://xxx.xxx.xx.xx/';
+var count = 10;
+
+for(var i = 1; i <= count; i++)
+{
+  request(url, function(err, res, body){
+    console.log(res.statusCode);
+  });
+}
+
