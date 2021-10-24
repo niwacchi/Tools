75a6427f4e30c4055ea6b0e0d7988b7a0544cd51
niwacchi <niwacchi@gmail.com>
2021-10-24 20:31:21 +0900

    Code update.

--- api-test/app.py
+++ api-test/app.py
@@ -1,6 +1,6 @@
 import requests
-import pprint
-from bottle import route, run, template, post, get, request
+import json
+from bottle import route, run, template, post, request
 from bottle import debug
 debug(True)
 
@@ -9,10 +9,18 @@ def index():
     return template('index.html', response='')
 
 @post('/get-api-response')
-def result():
+def get_result():
     url = request.forms.get('url')
     response = requests.get(url)
-    return template('index.html', response=response.text)
+    response = json.dumps(response.json(), indent=4)
+    return template('index.html', response=response)
+
+@post('post-api-response')
+def post_result():
+    url = request.forms.get('url')
+    response = requests.post(url)
+    response = json.dumps(response, indent=4)
+    return template('index.html', response=response)
 
 # reloaderにTrueをセットするとファイル更新で再起動する
 run(host='localhost', port=8080, reloader=True)
